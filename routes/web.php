4e82e44f432f13c9bcba5c605352b32ed46e5e82
riandroraul <riandroraul@gmail.com>
2024-05-02 17:56:05 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,23 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
+});
+
+// Route::get('/signup', function () {
+//     return view('signup');
+// });
+
+// Route::get('/signin', function () {
+//     return view('signin');
+// });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
