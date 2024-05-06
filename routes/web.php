3baddc88f610fdbdcd2b82c3044fb85801978284
riandroraul <riandroraul@gmail.com>
2024-05-06 08:25:55 +0700

    Revert "update"
    This reverts commit 4e82e44f432f13c9bcba5c605352b32ed46e5e82.

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('app');
-// });
+Route::get('/', function () {
+    return view('app');
+});
 
-// Route::get('/signup', function () {
-//     return view('signup');
-// });
+Route::get('/signup', function () {
+    return view('signup');
+});
 
-// Route::get('/signin', function () {
-//     return view('signin');
-// });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::get('/signin', function () {
+    return view('signin');
 });
